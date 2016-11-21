0ac0a5a88844e966ec7281e3dc901402ceb53c3a
Zech <fbicodex@gmail.com>
2016-11-20 23:44:24 -0600

    Added Item, Weapon, Consumable Changed character creation text "name" to be a hint so that the user doesn't have to erase it

---
+++ app/src/main/java/com/example/zech/simplerpg/Item.java
@@ -0,0 +1,18 @@
+package com.example.zech.simplerpg;
+
+/*
+ * Created by Zech on 11/20/2016.
+ */
+
+import android.media.Image;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Item implements Serializable
+{
+    public String name;
+    public Image icon;
+    public String description;
+
+}
