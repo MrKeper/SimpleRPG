c8f9153bc551c25d7c6c98b6d2c19ff253094ae5
Zech <fbicodex@gmail.com>
2016-11-30 00:07:27 -0600

    battle update #4 - Working AttackSlash animation

--- app/src/main/java/com/example/zech/simplerpg/Battle.java
+++ app/src/main/java/com/example/zech/simplerpg/Battle.java
@@ -1,12 +1,15 @@
 package com.example.zech.simplerpg;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -14,16 +17,16 @@ public class Battle extends AppCompatActivity {
 
     User_Character user;
     TextView userHPtext;
+    ImageView enemyHitImage;
     ProgressBar userHealthProgress;
 
     public void changeUserHealthBar(final int n){
         final Handler handler = new Handler();
 
-
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(user.current_health > Math.max(0,n)){
+                while(user.current_health >  Math.max(0,n)){
                     user.current_health = user.current_health - 1;
                     //final int tempHP = userHealthProgress.getProgress() - 1;
                     handler.post(new Runnable() {
@@ -41,7 +44,47 @@ public class Battle extends AppCompatActivity {
                 }
             }
         }).start();
+    }
 
+    public void userAttackAnimation(){
+        final Handler handler = new Handler();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(400);
+                }catch (Exception e){
+                    System.out.println("Sleep not working:"+userHealthProgress.getProgress());
+                }
+
+                int i = 11;
+                while(i <= 31){
+                    final int temp = i;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String name = "battle_slash"+temp;
+                            int id = getResources().getIdentifier(name, "drawable", getPackageName());
+                            enemyHitImage.setImageResource(id);
+                        }
+                    });
+                    try{
+                        Thread.sleep(80);
+                    }catch (Exception e){
+                        System.out.println("Sleep not working:"+userHealthProgress.getProgress());
+                    }
+                    i++;
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int id = getResources().getIdentifier("battle_slash0", "drawable", getPackageName());
+                        enemyHitImage.setImageResource(id);
+                    }
+                });
+            }
+        }).start();
     }
 
     @Override
@@ -51,11 +94,13 @@ public class Battle extends AppCompatActivity {
         user = (User_Character) getIntent().getSerializableExtra("user");
         userHPtext = (TextView)  findViewById(R.id.userHealthText);
         userHealthProgress = (ProgressBar) findViewById(R.id.userHealthBar);
+        enemyHitImage = (ImageView) findViewById(R.id.enemyHitImage);
 
         // Initialize stuff
         userHPtext.setText(user.current_health + " / " + user.base_health);
         userHealthProgress.setMax(user.base_health);
         userHealthProgress.setProgress(user.current_health);
+
 
 
         MediaPlayer mp  = MediaPlayer.create(this, R.raw.battle_music1);
@@ -69,6 +114,7 @@ public class Battle extends AppCompatActivity {
             public void onClick(View v) {
                 attackSound.start();
                 changeUserHealthBar(40);
+                userAttackAnimation();
                 //attackSound.stop();
             }
         });
