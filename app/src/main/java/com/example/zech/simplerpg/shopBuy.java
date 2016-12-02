404d8911e16d9351df95b633e3bb2cc3979d36a5
Julian <Jcisneros92@gmail.com>
2016-12-01 19:16:15 -0600

    Changed shopBuy to ShopBuy

--- app/src/main/java/com/example/zech/simplerpg/shopBuy.java
+++ app/src/main/java/com/example/zech/simplerpg/shopBuy.java
@@ -3,7 +3,7 @@ package com.example.zech.simplerpg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class shopBuy extends AppCompatActivity {
+public class ShopBuy extends AppCompatActivity {
 
     //Current_info temp2 = (Current_info) getIntent().getSerializableExtra("temp");
 
