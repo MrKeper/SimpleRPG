da2e79eafd3830f3d6a014fc4d2949918baa1f75
Keenan-P-Parker <keenanparker96@gmail.com>
2016-12-04 20:22:28 -0600

    quest in shop gone

--- app/src/main/java/com/example/zech/simplerpg/town1QuestBoard.java
+++ app/src/main/java/com/example/zech/simplerpg/town1QuestBoard.java
@@ -19,6 +19,8 @@ import static android.graphics.Color.YELLOW;
 public class town1QuestBoard extends AppCompatActivity {
     public ViewGroup layout;
     public MediaPlayer buttonSound;
+    public MediaPlayer questComplete;
+    public Boolean clearDungeon1Complete = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +29,19 @@ public class town1QuestBoard extends AppCompatActivity {
         final User_Character user = (User_Character) getIntent().getSerializableExtra("user");
         layout = (ViewGroup) findViewById(R.id.questBoardLayout);
         buttonSound  = MediaPlayer.create(this, R.raw.button_press);
+        questComplete = MediaPlayer.create(this,R.raw.winning);
         ArrayList<Integer> currentQuests = new ArrayList<>();
-        for(int i = 0; i < user.quest_list.size(); i++)
-        {
-            currentQuests.add(user.quest_list.get(i).quest_id);
-        }
         TextView questBoardText = new TextView(this);
         questBoardText.setText("\nAvaliable Quests: \n");
         questBoardText.setTextColor(WHITE);
         questBoardText.setTextSize(25);
         questBoardText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
         layout.addView(questBoardText);
-       /////////////////////////////////////////////// If quest,200 not in questList or finsihQuests
+
+        ////////////////////////////////////////////////////////////////////////////// Quest: ClearDungone1
         int[] bronzeStats = {0,3,0,0,0,0};
         Weapon bronzeSword = new Weapon("Bronze Sword",null,"A solid sword forged by the locals of Ironforge (+3 STR)",bronzeStats,15);
-        final Quest clearDungeon1 = new Quest(200,"Town in Need I",
+        final Quest clearDungeon1 = new Quest(100,"Town in Need I",
                 "Recently Ironforge has been having issues with monsters wondering out of the dungeon. This could be due to overpopulation. Clear the dungeon out to give them a hand.",
                 "Boss' Heart", 50,bronzeSword);
 
@@ -53,16 +53,39 @@ public class town1QuestBoard extends AppCompatActivity {
         quest_info.setTextSize(23);
         quest_info.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
         layout.addView(quest_info);
-
+        for(int i = 0; i < user.quest_list.size(); i++)
+        {
+            if(user.quest_list.get(i).quest_id == clearDungeon1.quest_id && user.quest_list.get(i).isComplete)
+            {
+                clearDungeon1Complete = true;
+            }
+            currentQuests.add(user.quest_list.get(i).quest_id);
+        }
         Button acceptQuestClearDungeon1 = new Button(this);
         acceptQuestClearDungeon1.setSoundEffectsEnabled(false);
-        if(currentQuests.contains(clearDungeon1.quest_id))
+        if(currentQuests.contains(clearDungeon1.quest_id) && !clearDungeon1Complete)
         {
             acceptQuestClearDungeon1.setText("Quest Taken");
         }
         else if(user.completedQuests.contains(clearDungeon1.quest_id))
         {
             acceptQuestClearDungeon1.setText("Quest Completed");
+        }
+        else if(clearDungeon1Complete && !user.completedQuests.contains(clearDungeon1.quest_id))
+        {
+            acceptQuestClearDungeon1.setText("Turn in Quest");
+            acceptQuestClearDungeon1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    questComplete.start();
+                    user.turnInQuest(clearDungeon1);
+                    Intent intent = new Intent(v.getContext(), town1QuestBoard.class);
+                    intent.putExtra("user",user);
+                    startActivity(intent);
+                    buttonSound.release();
+                    finishAfterSound(questComplete);
+                }
+            });
         }
         else
         {
@@ -71,6 +94,7 @@ public class town1QuestBoard extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     buttonSound.start();
+                    clearDungeon1.isComplete = true;
                     user.quest_list.add(clearDungeon1);
                     Intent intent = new Intent(v.getContext(), town1QuestBoard.class);
                     intent.putExtra("user",user);
@@ -81,7 +105,11 @@ public class town1QuestBoard extends AppCompatActivity {
 
         }
         layout.addView(acceptQuestClearDungeon1);
+        ////////////////////////////////////////////////////////////////////////////// Quest: insert quest here
 
+
+
+        ////////////////////////////////////////////////////////////////////////////// Quests above
         Button backButton = (Button) findViewById(R.id.backButtonQuestBoard);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,7 +130,7 @@ public class town1QuestBoard extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(mp.getDuration());
                 } catch (Exception e) {
                     System.out.println("finishAfterSound sleep ERROR");
                 }
@@ -117,4 +145,5 @@ public class town1QuestBoard extends AppCompatActivity {
             }
         }).start();
     }
+
 }
